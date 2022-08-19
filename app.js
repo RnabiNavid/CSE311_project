78dd6a3ca227195966341316a39e68d94a654eea
Rafiqun Nabi Torofder Navid <107410643+RnabiNavid@users.noreply.github.com>
2022-08-20 01:06:23 +0600

    js

--- app.js
+++ app.js
@@ -48,3 +48,15 @@ var repeat = function (activeClass) {
   repeater();
 };
 repeat();
+
+const signin_btn = document.querySelector("#signin-btn");
+const signup_btn = document.querySelector("#signup-btn");
+const container = document.querySelector(".container");
+
+signin_btn.addEventListener("click", () => {
+  container.classList.add("signup-mode");
+});
+
+signin_btn.addEventListener("click", () => {
+  container.classList.add("signup-mode");
+});
